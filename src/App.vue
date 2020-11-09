42378a0b8fdc9a642f41fb77cf27b26718e767f4
noble <noblewill2012@icloud.com>
2020-11-09 04:14:23 +0100

    single and muliple learn pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="bg-autocare-bg">
     <Header />
-    <Learn />
+    <SingleLearn />
     <Footer />
   </div>
 </template>
@@ -12,7 +12,9 @@ import Footer from "./components/Footer.vue";
 // import Home from "./components/Home.vue";
 // import About from "./components/About.vue";
 // import Contacts from "./components/Contacts.vue";
-import Learn from "./components/Learn.vue";
+// import Learn from "./components/Learn.vue";
+import SingleLearn from "./components/SingleLearn.vue";
+
 
 
 export default {
@@ -22,7 +24,8 @@ export default {
     Footer,
     // Home,
     // About,
-    Learn,
+    // Learn,
+    SingleLearn,
   },
 };
 </script>
---
+++ src/components/SingleLearn.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="px-10 py-16 sm:px-24 max-w-4xl mx-auto">
+    <div class="sm:px-24">
+      <h1 class="text-2xl font-bold leading-tight w-11/12 text-autocare-blue">
+        Five Simple Car Faults You Can Fix Yourself
+      </h1>
+
+      <div class="mt-4 flex items-center">
+        <div class="border-2 border-autocare-orange w-4 h-4 rounded-full"></div>
+        <div class="ml-2">
+          <p class="leading-tight text-xs text-autocare-blue">
+            <span class="font-semibold">The AutoCare Team,</span> Oct. 25, 2020
+            &middot; 5 min read
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-gray-500 h-32 sm:h-48 shadow-lg rounded-lg mt-10"></div>
+
+    <div class="mt-10 text-autocare-blue font-hairline sm:px-24">
+      <p>
+        Have you ever been in a rush and the car suddenly developed a fault and
+        wouldn’t start? Not to worry, these are every day car faults and should
+        be anticipated and prepared for. However there are particular repairs
+        that you can fix yourself whenever some parts of your car become faulty.
+        <br />
+        <br />
+        These repairs require no special tools neither do they need professional
+        expertise. Five of these repairs are:
+        <br />
+        <br />
+        <span class="font-semibold">Dead battery:</span> this usually happen
+        when you leave the headlights on as this drains the battery power while
+        you were away. This incident does not require the service of an
+        expatriate because it is something you can do yourself. Simply connect
+        the positive lead to the appropriate terminal on the battery while the
+        negative lead is connected to the chassis or engine ground. Once this is
+        done, turn on the pack and start your car. Put your jump pack on the
+        included charger overnight to get it charged for subsequent times.
+        <br />
+        In the absence of a jump pack, get another car to help you jump start
+        your car while you revive it to life.
+        <br />
+        <br />
+        Tools needed: Jump pack, your hands.
+        <br />
+        <br />
+        <span class="font-semibold">Flat Tire:</span> a tire deflates without a
+        forewarning; you should be used to that by now. Flat tires are a result
+        of running over sharp objects that puncture the tire. Thanks to spare
+        tire that always come to the rescue. All that is needed in this instance
+        is; first secure the car with wheel wedges against each side of the tire
+        you are changing. Then, jack up the car, unscrew the bolts that held the
+        tire in place, replace the old tire with the spare and fix the bolts
+        back.
+        <br />
+        <br />
+        In the absence of a spare, always have a can of tire sealant to seal up
+        the puncture in the tire, take you to safety while your mechanic works
+        on further replacement or replacement.
+      </p>
+    </div>
+
+    <div class="mt-20 sm:mt-32">
+      <div class="bg-white rounded-lg shadow-xl px-4 py-8 sm:px-12 sm:py-10">
+        <div class="px-8 sm:px-12">
+          <h1 class="text-sm leading-tight font-semibold text-autocare-blue">
+            Join our email list and get notified about new content
+          </h1>
+          <p
+            class="text-xs leading-tight mt-2 sm:mt-4 font-hairline text-gray-600"
+          >
+            Be the first to receive our latest content with the ability to
+            opt-out at anytime. We promise to not spam your inbox or share your
+            email with any third parties.
+          </p>
+        </div>
+        <div class="sm:px-20">
+          <div class="relative mt-5">
+            <input
+              type="text"
+              name=""
+              id=""
+              class="w-full rounded-full bg-gray-200 text-xs px-4 py-1 font-hairline"
+              placeholder="Enter your Email"
+            />
+            <button
+              class="text-xs border border-autocare-blue rounded-full px-4 absolute right-0 mt-1 mr-2"
+            >
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        <div class="flex items-center mt-20 justify-between">
+          <p class="font-hairline text-xs text-gray-600 w-7/12 sm:w-4/12">
+            More articles from the blog
+          </p>
+          <div class="border-b border-gray-400 w-5/12 sm:w-8/12"></div>
+        </div>
+
+        <div class="sm:flex">
+          <div class="mt-10 sm:w-7/12">
+            <div class="sm:w-11/12">
+              <div class="w-full bg-gray-500 h-40 rounded-lg shadow-lg"></div>
+              <h3
+                class="text-lg font-semibold leading-tight mt-4 text-autocare-blue"
+              >
+                Ten common faults associated with Toyota Cars and what to do
+              </h3>
+
+              <p class="mt-3 sm:mt-3 text-xs font-hairline">
+                Toyota Company has managed to stay relevant in the vehicle
+                industry over the decades...
+              </p>
+            </div>
+          </div>
+
+          <div class="mt-10 sm:w-5/12">
+            <div class="sm:w-11/12">
+              <div class="w-full bg-gray-500 h-40 rounded-lg shadow-lg"></div>
+              <h3
+                class="text-lg font-semibold leading-tight mt-4 text-autocare-blue"
+              >
+                How to wash your car to keep the body from fading
+              </h3>
+
+              <p class="mt-3 sm:mt-3 text-xs font-hairline">
+                It is quite painful to have successfully purchased your dream
+                car and then
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
